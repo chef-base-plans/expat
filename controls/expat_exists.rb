82f948d8dcbb814625d5ee8515308df268181f14
Gavin Didrichsen <gavin.didrichsen@gmail.com>
2020-06-08 15:52:25 +0100

    Update with description and format
    Signed-off-by: Gavin Didrichsen <gavin.didrichsen@gmail.com>

--- controls/expat_exists.rb
+++ controls/expat_exists.rb
@@ -1,17 +1,23 @@
 title 'Tests to confirm expat exists'
 
+plan_origin = ENV['HAB_ORIGIN']
 plan_name = input('plan_name', value: 'expat')
-plan_ident = "#{ENV['HAB_ORIGIN']}/#{plan_name}"
-expat_relative_path = input('command_path', value: '/bin/xmlwf')
-expat_installation_directory = command("hab pkg path #{plan_ident}")
-expat_full_path = expat_installation_directory.stdout.strip + "#{ expat_relative_path}"
  
 control 'core-plans-expat-exists' do
   impact 1.0
   title 'Ensure expat exists'
   desc '
-  '
-   describe file(expat_full_path) do
+  Verify expat by ensuring /bin/xmlwf exists'
+  
+  plan_installation_directory = command("hab pkg path #{plan_origin}/#{plan_name}")
+  describe plan_installation_directory do
+    its('exit_status') { should eq 0 }
+    its('stdout') { should_not be_empty }
+  end
+
+  command_relative_path = input('command_relative_path', value: '/bin/xmlwf')
+  command_full_path = plan_installation_directory.stdout.strip + "#{command_relative_path}"
+  describe file(command_full_path) do
     it { should exist }
   end
 end
--- controls/expat_works.rb
+++ controls/expat_works.rb
@@ -1,33 +1,32 @@
 title 'Tests to confirm expat works as expected'
 
+plan_origin = ENV['HAB_ORIGIN']
 plan_name = input('plan_name', value: 'expat')
-plan_ident = "#{ENV['HAB_ORIGIN']}/#{plan_name}"
-expat_path = command("hab pkg path #{plan_ident}")
-expat_pkg_ident = ((expat_path.stdout.strip).match /(?<=pkgs\/)(.*)/)[1]
+plan_installation_directory = command("hab pkg path #{plan_origin}/#{plan_name}")
+plan_pkg_ident = ((plan_installation_directory.stdout.strip).match /(?<=pkgs\/)(.*)/)[1]
+plan_pkg_version = (plan_pkg_ident.match /^#{plan_origin}\/#{plan_name}\/(?<version>.*)\//)[:version]
 
-control 'core-plans-expat-works-001' do
+control 'core-plans-expat-works' do
   impact 1.0
-  title 'expat binary should exist'
-  describe expat_path do
+  title 'Ensure expat works as expected'
+  desc '
+  Verify expat by ensuring (1) its installation directory exists and (2) that
+  it successfully parses valid xml and (3) that it reports an error for invalid
+  xml
+  '
+  
+  describe plan_installation_directory do
     its('exit_status') { should eq 0 }
     its('stdout') { should_not be_empty }
   end
-end
 
-control 'core-plans-expat-works-002' do
-  impact 1.0
-  title 'expat should parse well-formed xml'
-  describe command("DEBUG=true; hab pkg exec #{expat_pkg_ident} xmlwf /hab/svc/expat/config/fixtures/valid.xml") do
+  describe command("DEBUG=true; hab pkg exec #{plan_pkg_ident} xmlwf /hab/svc/expat/config/fixtures/valid.xml") do
     its('exit_status') { should eq 0 }
     its('stdout') { should be_empty }
     its('stderr') { should be_empty }
   end
-end
 
-control 'core-plans-expat-works-003' do
-  impact 1.0
-  title 'should report error on malformed xml'
-  describe command("DEBUG=true; hab pkg exec #{expat_pkg_ident} xmlwf /hab/svc/expat/config/fixtures/mismatched-tag.xml") do
+  describe command("DEBUG=true; hab pkg exec #{plan_pkg_ident} xmlwf /hab/svc/expat/config/fixtures/mismatched-tag.xml") do
     its('exit_status') { should eq 0 }
     its('stdout') { should_not be_empty }
     its('stdout') { should match /mismatched tag/ }
