3ab5f39170cbca2372cc56f4ca91cf713f7926c5
Gavin Didrichsen <gavin.didrichsen@gmail.com>
2020-06-16 16:42:27 +0100

    Update documentation, removing DEBUG, etc
    Signed-off-by: Gavin Didrichsen <gavin.didrichsen@gmail.com>

--- controls/expat_works.rb
+++ controls/expat_works.rb
@@ -2,37 +2,45 @@ title 'Tests to confirm expat works as expected'
 
 plan_origin = ENV['HAB_ORIGIN']
 plan_name = input('plan_name', value: 'expat')
-plan_installation_directory = command("hab pkg path #{plan_origin}/#{plan_name}")
-plan_pkg_ident = ((plan_installation_directory.stdout.strip).match /(?<=pkgs\/)(.*)/)[1]
-plan_pkg_version = (plan_pkg_ident.match /^#{plan_origin}\/#{plan_name}\/(?<version>.*)\//)[:version]
 
 control 'core-plans-expat-works' do
   impact 1.0
   title 'Ensure expat works as expected'
   desc '
-  Verify expat by ensuring (1) its installation directory exists and (2) that
-  it successfully parses valid xml and (3) that it reports an error for invalid
-  xml
+  Verify expat by ensuring that
+  (1) its installation directory exists 
+  (2) it returns the expected version
+  (3) it successfully parses valid xml <doc>One</doc> 
+  (4) it reports an error for invalid xml <doc>Two<doc>
   '
   
+  plan_installation_directory = command("hab pkg path #{plan_origin}/#{plan_name}")
   describe plan_installation_directory do
     its('exit_status') { should eq 0 }
     its('stdout') { should_not be_empty }
+    its('stderr') { should be_empty }
+  end
+  
+  command_relative_path = input('command_relative_path', value: 'bin/xmlwf')
+  command_full_path = File.join(plan_installation_directory.stdout.strip, command_relative_path)
+  plan_pkg_version = plan_installation_directory.stdout.split("/")[5]
+  describe bash("#{command_full_path} -v") do
+    its('exit_status') { should eq 0 }
+    its('stdout') { should_not be_empty }
+    its('stdout') { should match /xmlwf using expat_#{plan_pkg_version}/ }
+    its('stderr') { should be_empty }
   end
 
-  describe command("DEBUG=true; hab pkg exec #{plan_pkg_ident} xmlwf /hab/svc/expat/config/fixtures/valid.xml") do
+  describe bash("#{command_full_path} < <(echo '<doc>One</doc>')") do
     its('exit_status') { should eq 0 }
     its('stdout') { should be_empty }
     its('stderr') { should be_empty }
   end
 
-  describe command("DEBUG=true; hab pkg exec #{plan_pkg_ident} xmlwf /hab/svc/expat/config/fixtures/mismatched-tag.xml") do
+  describe bash("#{command_full_path} < <(echo '<doc>Two<doc>')") do
     its('exit_status') { should eq 0 }
     its('stdout') { should_not be_empty }
-    its('stdout') { should match /mismatched tag/ }
+    its('stdout') { should match /no element found/ }
     its('stderr') { should be_empty }
   end
-
 end
-
-
